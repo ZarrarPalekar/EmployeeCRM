b9e4e1f572dd5968c86a66e44f078efb82759e0d
Zarrar Jawed Ahmad Palekar <zarrar.palekar@syndicationpro.com>
2022-10-02 15:53:24 +0530

    code refactoring

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function NotFoundPage() {
   return (
     <>
-      <h1 style={{ color: "white", marginBottom: "10px" }}>404 Not Found!</h1>{" "}
+      <h1 className="text-black mb-10">404 - Page Not Found!</h1>{" "}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         <Link to="/">Go Back</Link>
       </button>
